8bb9e4030178b4ed43eab7586648ca3f7cc5797a
Netanel Harush <netanelharush2@gmail.com>
2025-06-08 09:16:01 +0300

    feat: implement LocatorUtils and envUtils for improved locator handling and environment variable management; refactor BasePage to utilize new utilities

--- src/core/BasePage.ts
+++ src/core/BasePage.ts
@@ -1,63 +1,20 @@
+import {LocatorUtils} from '@/core/LocatorUtils'
 import type {Locator, Page} from '@playwright/test'
 import {expect} from '@playwright/test'
 
 type stringOrRoleLocatorType = string | {role: string; name: string}
 
-export class BasePage {
-  protected page: Page
-
+export class BasePage extends LocatorUtils {
   constructor(page: Page) {
-    this.page = page
+    super(page)
   }
-  // Find a locator by string, using multiple strategies
-  private _findLocatorByString(locator: string): Locator {
-    // Try each method in sequence and return the first one that works
-    const strategies = [
-      () => this.page.locator(locator),
-      () => this.page.getByLabel(locator),
-      () => this.page.getByText(locator),
-    ]
-    for (const strategy of strategies) {
-      try {
-        return strategy()
-      } catch {
-        continue
-      }
-    }
-    throw new Error(`Unable to find element with string locator: ${locator}`)
-  }
-  // Handle Generic Locator, byRole, byLabel, byText
-  private _extractLocator(locator: stringOrRoleLocatorType): Locator {
-    try {
-      // Try generic locator
-      if (typeof locator === 'string') {
-        return this._findLocatorByString(locator)
-        // Try role and name
-      } else if (
-        typeof locator === 'object' &&
-        locator !== null &&
-        'role' in locator &&
-        'name' in locator
-      ) {
-        const {role, name} = locator
-        return this.page.getByRole(role as any, {
-          name: name,
-        })
-      } else {
-        throw new Error(
-          `Locator "${locator}" is not a valid string or role object.`,
-        )
-      }
-    } catch {
-      throw new Error(`Unable to find element with locator: ${locator}`)
-    }
-  }
+
   // Validate text of an element
   protected async validateText(
     locator: stringOrRoleLocatorType,
     text: string,
   ): Promise<void> {
-    const extractedLocator = this._extractLocator(locator)
+    const extractedLocator = this.extractLocator(locator)
     try {
       await expect(extractedLocator).toHaveText(text)
     } catch {
@@ -82,12 +39,12 @@ export class BasePage {
   protected async clickOnElement(
     locator: stringOrRoleLocatorType,
   ): Promise<void> {
-    const extractedLocator: Locator = this._extractLocator(locator)
+    const extractedLocator: Locator = this.extractLocator(locator)
     await extractedLocator.click()
   }
   // Fill an input field with text
   protected async fillInput(locator: string, text: string): Promise<void> {
-    const extractedLocator: Locator = this._extractLocator(locator)
+    const extractedLocator: Locator = this.extractLocator(locator)
     await extractedLocator.fill(text)
   }
 }
