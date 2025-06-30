0ae14dc0aac4aff6b9a87dc08bff004946b8f08a
Netanel Harush <netanelharush2@gmail.com>
2025-06-30 17:12:27 +0300

    feat: integrate husky and lint-staged for pre-commit and pre-push hooks; add code quality checks in GitHub Actions

--- src/tests/sliders-main-page.spec.ts
+++ src/tests/sliders-main-page.spec.ts
@@ -2,19 +2,19 @@ import test from '@/fixtures/testSetup'
 test.describe.serial('Sliders Main Page Tests', () => {
   test.describe.configure({timeout: 60000})
 
-  test('should validate the first slider ', async ({mainPage}) => {
+  test('should validate the first slider @smoke', async ({mainPage}) => {
     await mainPage.slide1Verification()
   })
-  test('should validate the second slider', async ({mainPage}) => {
+  test('should validate the second slider @smoke', async ({mainPage}) => {
     await mainPage.slide2Verification()
   })
-  test('should validate the third slider', async ({mainPage}) => {
+  test('should validate the third slider @smoke', async ({mainPage}) => {
     await mainPage.slide3Verification()
   })
-  test('should validate the fourth slider', async ({mainPage}) => {
+  test('should validate the fourth slider @smoke', async ({mainPage}) => {
     await mainPage.slide4Verification()
   })
-  test('should validate the fifth slider', async ({mainPage}) => {
+  test('should validate the fifth slider @smoke', async ({mainPage}) => {
     await mainPage.slide5Verification()
   })
 })
