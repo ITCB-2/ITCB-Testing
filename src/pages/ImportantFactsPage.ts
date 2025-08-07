8728c0ae73e4cc66a6c063eb5b585abf6caa0fb8
Netanel Harush <netanelharush2@gmail.com>
2025-08-07 16:43:22 +0300

    Refactor validateImportantFact method to use FactName type for improved type safety

--- src/pages/ImportantFactsPage.ts
+++ src/pages/ImportantFactsPage.ts
@@ -3,15 +3,14 @@ import test from '@/fixtures/testSetup'
 import {findItemByProperty} from '@/helpers/arrayUtils'
 import {IMPORTANT_FACTS_PAGE_LOCATORS} from '@/locators/Important_Facts'
 import {type Page} from '@playwright/test'
-import type {factNames} from '../data/importantFactsPageData'
+
+type FactName = (typeof IMPORTANT_FACTS_PAGE_LOCATORS.facts)[number]['name']
 
 export class ImportantFactsPage extends BasePage {
   constructor(page: Page) {
     super(page)
   }
-  async validateImportantFact(
-    factName: (typeof factNames)[number],
-  ): Promise<void> {
+  async validateImportantFact(factName: FactName): Promise<void> {
     await test.step(`Validate Important Fact ${factName}`, async () => {
       const {facts} = IMPORTANT_FACTS_PAGE_LOCATORS
       const fact = findItemByProperty(facts, 'name', factName)
