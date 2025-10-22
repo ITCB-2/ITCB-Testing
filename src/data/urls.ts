5173aaf69929cac5f3b891cd0a561d86ec15abc3
Netanel Harush <netanelharush2@gmail.com>
2025-10-22 19:20:19 +0300

    fix: update playwright-framework to version 6.0.0 and simplify BASE_URL retrieval

--- src/data/urls.ts
+++ src/data/urls.ts
@@ -1,6 +1,3 @@
 import {getEnvCredentials} from '@netanelh2/playwright-framework'
-import dotenv from 'dotenv'
 
-dotenv.config({quiet: true})
-
-export const BASE_URL: string = getEnvCredentials(process.env.BASE_URL)
+export const BASE_URL: string = getEnvCredentials('BASE_URL')
